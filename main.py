24934603936a2eb38de1a3653b3d63403cc961be
Andrey Glumov <gluman@yandex.ru>
2022-10-29 18:03:38 +0300

    added: change name of obj def _abs_score_student in class Student def _list_courses in class Student def _list_finished_courses in class Student def __str__ in class Student def _abs_score_lector in class Lecturer def __str__ in class Lecturer

--- main.py
+++ main.py
@@ -10,15 +10,41 @@ class Student:
     def rate_lc(self, lector, course, grade):
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if 1 <= grade <= 10:
-               if course in lector.lectorgrades:
-                   lector.lectorgrades[course] += [grade]
-               else:
-                   lector.lectorgrades[course] = [grade]
+                if course in lector.lectorgrades:
+                    lector.lectorgrades[course] += [grade]
+                else:
+                    lector.lectorgrades[course] = [grade]
             else:
-               return 'Ошибка'
+                return 'Ошибка'
         else:
             return 'Ошибка'
 
+    def _abs_score_student(self):
+        sum_score, count_score = 0, 0
+        for score in self.grades.values():
+            for i in score:
+                sum_score += i
+                count_score += 1
+
+        abs_score = sum_score / count_score
+        return abs_score
+
+    def _list_courses(self):
+        cources_str = ",".join(self.courses_in_progress)
+        return cources_str
+
+    def _list_finished_courses(self):
+        finished_courses_str = ', '.join(self.finished_courses)
+        return finished_courses_str
+
+    def __str__(self):
+        return f'''
+            Имя: {self.name}
+            Фамилия: {self.surname}
+            Средняя оценка за лекции: {self._abs_score_student():.1f}
+            Курсы в процессе изучения: {self._list_courses()}
+            Завершенные курсы: {self._list_finished_courses()}
+            '''
 
 class Mentor:
     def __init__(self, name, surname):
@@ -32,6 +58,22 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.lectorgrades = {}
 
+    def _abs_score_lector(self):
+        sum_score, count_score = 0, 0
+        for score in self.lectorgrades.values():
+            for i in score:
+                sum_score += i
+                count_score += 1
+
+        abs_score = sum_score / count_score
+        return abs_score
+
+    def __str__(self):
+        return f'''
+            Имя: {self.name}
+            Фамилия: {self.surname}
+            Средняя оценка за лекции: {self._abs_score_lector():.1f}'''
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -41,30 +83,35 @@ class Reviewer(Mentor):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
-
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'''
+            Имя: {self.name}
+            Фамилия: {self.surname}'''
 
 best_student = Student('Andrey', 'Glumov', 'Male')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['C++']
 
-cool_reviewer = Reviewer('Robo', 'Cop')
-cool_reviewer.courses_attached += ['Python']
+some_reviewer = Reviewer('Robo', 'Cop')
+some_reviewer.courses_attached += ['Python']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 5)
-# cool_reviewer.rate_hw(best_student, 'Python', 10)
-
-
-main_lector = Lecturer('Term', 'Nator')
-main_lector.courses_attached += ['Python']
-
-best_student.rate_lc(main_lector, 'Python', 10)
-best_student.rate_lc(main_lector, 'Python', 1)
+some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 5)
+some_reviewer.rate_hw(best_student, 'Python', 7)
 
 
-print(best_student.grades)
-print(main_lector.lectorgrades)
+some_lector = Lecturer('Termi', 'Nator')
+some_lector.courses_attached += ['Python']
+
+best_student.rate_lc(some_lector, 'Python', 10)
+best_student.rate_lc(some_lector, 'Python', 5)
+
+print(some_reviewer)
+print(some_lector)
+print(best_student)
