492a167c66d3554cbc3da9c5199b38e8cf7b5e0e
Andrey Glumov <gluman@yandex.ru>
2022-10-29 12:09:48 +0300

    added: def rate_lc in class Stident class Lecturer class Reviewer relocate def rate_hw obj cool_reviewer, main_lecto

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lc(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
+            if 1 <= grade <= 10:
+               lector.courses_attached[course] += [grade]
+            else:
+               return 'Ошибка'
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lectorgrades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -24,14 +44,24 @@ class Mentor:
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Andrey', 'Glumov', 'Male')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Robo', 'Cop')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+
+main_lector = Lecturer('Term', 'Nator')
+main_lector.courses_attached += ['Python']
+
+best_student.rate_lc(main_lector, 'Python', 10)
+best_student.rate_lc(main_lector, 'Python', 1)
+
+
+
 
 print(best_student.grades)
+print()
